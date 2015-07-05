9f1a1135e6eacb57e950ad1de58ee87d13230a38
ks <a@a.com>
2015-07-05 23:55:17 +0200

    -

---
+++ js/custom.js
@@ -0,0 +1,6 @@
+$(function () {
+    $('a.email').click(function(e) {
+        e.preventDefault();
+        window.location.replace("ma" + "il" + "to" + ":" + "kresimir." + "sencaj" + "@" + "g" + "m" + "a" + "i" + "l" + "." + "c" + "o" + "m");
+    });
+});
